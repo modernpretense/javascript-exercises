a473237f7bc1fb901c77cd4a8d1c84e63e3ec6fc
Morten <7154135+modernpretense@users.noreply.github.com>
2023-02-21 18:37:59 +0100

    02 finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+const repeatString = function(input, amount) {
+    
+    let output = input;
+
+    for (let i = 0; i < amount-1; i++) {
+        output = output+input;
+    }
+
+    if (amount === 0) {
+        return '';
+    } else if (amount < 0) {
+        return "ERROR";
+    } else {
+        return output;
+    }
+    
 
 };
 
