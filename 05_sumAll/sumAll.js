f915d88ad086eb0cc7202ec8e1388d3a6a4d895d
Morten <7154135+modernpretense@users.noreply.github.com>
2023-02-23 12:57:30 +0100

    05 finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
 
+    let smallestNum;
+    let largetstNum;
+
+    if (numOne < 0 || numTwo < 0) {
+        return 'ERROR';
+    } else if (typeof numOne != 'number' || typeof numTwo != 'number') {
+        return 'ERROR';
+    } else if (numOne < numTwo) {
+        smallestNum = numOne;
+        largetstNum = numTwo;
+    } else if (numTwo < numOne) {
+        smallestNum = numTwo;
+        largetstNum = numOne;
+    } else {
+        return 'ERROR';
+    }
+
+    let sum = smallestNum;
+    let nextNum = smallestNum+1;
+
+    for (let i = smallestNum; i < largetstNum; i++) {
+        sum = sum + nextNum;
+        nextNum++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+ 1 , 4
